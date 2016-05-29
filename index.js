8a9fed61be2c8a0631b9c52c5080b4dd974c234f
Yvonne Hayes <queyvonne@gmail.com>
2016-05-29 16:03:58 -0700

    changed index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var server = require('./lib/server.js');
+const server = require('./lib/server.js');
+const DB_URI = process.env.DB_URI || 'mongodb://localhost/books';
+const database = require('./lib/database');
 
 // starting the server
-
-server.start((err) => {
-  if(err) {
-    throw err;
+server.start(err => {
+  if(err) console.log('ERROR at server start:', err);
+  else {
+    database.connect(DB_URI);
+    console.log('Server running at:', server.info.uri);
   }
-  console.log('Server running at:', server.info.uri);
 });
