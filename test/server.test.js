a98747afef86499ad773062605406bcbb949d005
Yvonne Hayes <queyvonne@gmail.com>
2016-05-31 08:02:28 -0700

    added test data to tests

--- test/server.test.js
+++ test/server.test.js
@@ -7,10 +7,13 @@ const Books = require('../models/books.js');
 const server = require('../lib/server.js');
 const mongoose = require('../lib/mongoose.js');
 
-const DB_URI = process.env.DB_URI || 'mongodb://localhost/godzilla';
+const testData = {
+  title: 'Pride and Prejudice',
+  author: 'Jane_Austen',
+  done: true,
+  rating: 9
+};
 
-describe('E2E tests', () => {
-
-
-
+describe('E2E testing', () => {
+  
 });
