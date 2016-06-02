f7ac471ad43a0dc2aa88dbf447384fa3c3f4ad52
Yvonne Hayes <queyvonne@gmail.com>
2016-06-02 13:07:04 -0700

    started on E2E testing. Testing POST works

--- test/server.test.js
+++ test/server.test.js
@@ -7,13 +7,35 @@ const Books = require('../models/books.js');
 const server = require('../lib/server.js');
 const mongoose = require('../lib/mongoose.js');
 
-const testData = {
-  title: 'Pride and Prejudice',
-  author: 'Jane_Austen',
-  done: true,
-  rating: 9
-};
+const DB_URI = process.env.DB_URI || 'mongodb://localhost/books';
+
 
 describe('E2E testing', () => {
-  
+
+  mongoose.connect(DB_URI);
+
+  const testData = {
+    title: 'Pride and Prejudice',
+    author: 'Jane_Austen',
+    done: true,
+    rating: 9
+  };
+
+  before(done => {
+    Books.remove({})
+      .then(done());
+  });
+
+  it('TestData gets posted to /books', done => {
+    const reqOptions = {
+      method: 'POST',
+      url: '/books',
+      payload: testData
+    };
+    server.inject(reqOptions, res => {
+      assert.equal(res.statusCode, 200);
+      done();
+    });
+  });
+
 });
